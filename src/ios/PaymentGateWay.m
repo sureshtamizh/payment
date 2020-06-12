746553f3efdb33cc8a12ac405f36455055415720
sureshtamizh <sureshe424@gmail.com>
2020-06-12 15:18:29 +0530

    Intial commit

---
+++ src/ios/PaymentGateWay.m
@@ -0,0 +1,28 @@
+/********* PaymentGateWay.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface PaymentGateWay : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation PaymentGateWay
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
